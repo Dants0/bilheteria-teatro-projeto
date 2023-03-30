81304dede0177a52cf49a8d6a20b9550cb6cfd2b
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2023-03-30 14:01:14 -0300

    add sobre page

--- web/src/components/About.jsx
+++ web/src/components/About.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
+import './about.css'
+
 const About = () => {
     return (
         <div className="container_about">
             <h3>
                 Nosso objetivo é proporcionar uma experiência fácil e agradável de compra de ingressos, com um processo de compra rápido e seguro. Além disso, oferecemos uma variedade de opções de pagamento para atender às suas necessidades.
             </h3>
-            <p>
+            <div className="box_about">
+                <p>
 
-                Com nossa equipe altamente qualificada de especialistas em teatro, estamos sempre à frente das últimas tendências e novidades do mundo teatral. Isso nos permite oferecer a você uma seleção das melhores peças e produções disponíveis no mercado.
+                    Com nossa equipe altamente qualificada de especialistas em teatro, estamos sempre à frente das últimas tendências e novidades do mundo teatral. Isso nos permite oferecer a você uma seleção das melhores peças e produções disponíveis no mercado. <br/>
 
-                Ao escolher o nosso site de bilheteria de teatro, você pode ter a certeza de que encontrará os ingressos perfeitos para as suas necessidades. Desde espetáculos musicais a comédias, dramas e muito mais, temos tudo o que você precisa para aproveitar ao máximo a sua experiência teatral.
+                    Ao escolher o nosso site de bilheteria de teatro, você pode ter a certeza de que encontrará os ingressos perfeitos para as suas necessidades. Desde espetáculos musicais a comédias, dramas e muito mais, temos tudo o que você precisa para aproveitar ao máximo a sua experiência teatral.<br/>
 
-                Não perca mais tempo procurando por ingressos em sites menos confiáveis. Escolha o nosso site de bilheteria de teatro e comece a aproveitar tudo o que o mundo teatral tem a oferecer. Compre seus ingressos agora e prepare-se para uma noite inesquecível!
-            </p>
+                    Não perca mais tempo procurando por ingressos em sites menos confiáveis. Escolha o nosso site de bilheteria de teatro e comece a aproveitar tudo o que o mundo teatral tem a oferecer. Compre seus ingressos agora e prepare-se para uma noite inesquecível!
+                </p>
+            </div>
         </div>
     )
 }
